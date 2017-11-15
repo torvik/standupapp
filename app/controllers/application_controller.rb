499be29b6705cbed1ae7573bce3c144cb5a78239
torvik <pless.victor@gmail.com>
2017-11-15 18:29:17 +0000

    pag 118

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,13 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   layout :layout_by_resource
 
+  helper_method :current_account
+
+  def current_account
+    @current_account ||= current_user.account 
+    @current_account
+  end
+
   protected
 
   def layout_by_resource
